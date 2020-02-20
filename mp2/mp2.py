97dea90a91939c22cd36db9c523c4c3822c6eae1
Jeff Schulz <jeffschulz7@gmail.com>
2020-02-20 10:32:40 -0600

    gonna be late...

--- mp2/mp2.py
+++ mp2/mp2.py
@@ -26,16 +26,17 @@ def mean(data):
 
 
 # Manually Compute covariance matrix
-def cov(data,means):
+def cov(data):
     n = len(data[0])
     d = len(data)
     covs = np.zeros((d,d))
+    means = mean(data)
 
     for x in range(d):
         for y in range(d):
-            x_v = (data[x] - means[x]) / n
-            y_v = (data[y] - means[y]) / n
-            covs[x,y] = np.sum(x_v * y_v)
+            x_v = (data[x] - means[x])
+            y_v = (data[y] - means[y])
+            covs[x,y] = np.sum(x_v * y_v) / n
 
     return covs
 
@@ -50,7 +51,7 @@ def mahala(x1, x2, cov):
 def g_1b(x,data,prior):
     d  = len(data)
     mn = mean(data)
-    cv = cov(data,mn)
+    cv = cov(data)
     md = mahala(x,mn,cv)
 
     gi = (-1 * .5 * md) + (-1 * d/2 * np.log(2*np.pi)) + (-.5 * np.log(np.linalg.det(cv))) + np.log(prior)
@@ -72,7 +73,7 @@ def classifier(scores):
 # PDF
 def pdf(x,data,cov):
     det = np.linalg.det(cov)
-    temp = 1 / (np.power(2*np.pi , len(x)/2) * np.power(det , .5))
+    temp = 1 / (np.power(2*np.pi , len(data)/2) * np.power(det , .5))
     return temp * np.exp((-.5 * mahala(x,mean(data),cov)))
 
 
@@ -170,23 +171,245 @@ colors = ['magenta','yellow','cyan','red']
 for i,c in enumerate(test_samples): 
     ax.scatter3D(c[0],c[1],c[2],color=colors[i])
 
-#plt.show()
+plt.show()
 
+
+# Discriminant fx for b and d
+def case2(x,data,prior):
+    mn = mean(data)
+    cv = cov(data)
+    md = mahala(x,mn,cv)
+
+    return (-.5 * md) + np.log(prior)
+
+def case2_line(x,data1, data2):
+    mn1 = mean(data1)
+    mn2 = mean(data2)
+    cv = cov(data1)
+    w = np.dot( np.linalg.inv(cv) , (mn1 - mn2) )
+    x0 = (.5 * (mn1 + mn2))
+    #return np.dot( w.T , (x - x0))
+    return (-w[0] * (x - x0[0])) / w[1] + x0[1]
+
+# Discriminant fx for e
+def case3(x,data,prior):
+    mn = []
+    cv = []
+    md = []
+    for d in data:
+        mn.append(mean(d))
+        cv.append(cov(d))
+
+    g = []
+    for m,c in zip(mn,cv):
+        c_i = np.linalg.inv(c)
+        c_d = np.linalg.det(c)
+        bigw   = (-.5 * c_i)
+        smallw = np.dot(c_i , m) 
+        w0     = (-.5 * np.dot(np.dot(m.T , c_i), m)) - (.5 * np.log(c_d)) + np.log(prior)
+        g.append( np.dot(np.dot(x.T , bigw) , x) + np.dot(smallw.T , x) + w0 )
+
+    return g
 
 
 ###################################################
 # Question 2
 ###################################################
-data1 = np.random.multivariate_normal([8,2],[[4.1,0],[0,2.8]],1000).T
-data2 = np.random.multivariate_normal([2,8],[[4.1,0],[0,2.8]],1000).T
+dim = 1000
+data1 = np.random.multivariate_normal([8,2],[[4.1,0],[0,2.8]],dim).T
+data2 = np.random.multivariate_normal([2,8],[[4.1,0],[0,2.8]],dim).T
 
-m1 = mean(data1)
-m2 = mean(data2)
-print(m1, m2)
-print(cov(data1,m1))
-print(cov(data2,m2))
+cov1 = cov(data1)
+cov2 = cov(data2)
 
-print(np.cov(data1))
+print('Sampling PDFs...')
+# x_1, y_1 = data1[0], data1[1]
+# x_2, y_2 = data2[0], data2[1]
+x_1 = np.arange(-5, 15, .5)
+x_2 , y_1, y_2 = x_1 , x_1, x_1
+pdf1 = []
+pdf2 = []
+# for x1,x2 in zip(x_1,y_1):
+#     pdf1.append(pdf([x1,x2],data1,cov1))
+
+# for x1,x2 in zip(x_2,y_2):
+#     pdf2.append(pdf([x1,x2],data2,cov2))
+
+for x1 in x_1:
+    temp = []
+    for x2 in y_1:
+        temp.append(pdf([x1,x2],data1,cov1))
+    pdf1.append(temp)
+
+for x1 in x_2:
+    temp = []
+    for x2 in y_2:
+        temp.append(pdf([x1,x2],data2,cov2))
+    pdf2.append(temp)
+
+pdf1 = np.array(pdf1)
+pdf2 = np.array(pdf2)
+
+# clip data which is below 0.0
+pdf1[pdf1 < 0.0001] = np.NaN
+pdf2[pdf2 < 0.0001] = np.NaN
+
+print('Done.')
+
+x_1 = np.outer(x_1, np.ones(40))
+x_2 = np.outer(x_2, np.ones(40))
+y_1 = np.outer(np.ones(40),y_1)
+y_2 = np.outer(np.ones(40),y_2)
+
+
+# Plot hyperplane
+hp_y = []
+hp_x = np.arange(-5, 15, 1)
+for xi in hp_x:
+    hp_y.append(case2_line(xi,data1,data2))
+
+
+ax = plt.axes(projection='3d')
+pdf1 = np.ma.array(pdf1, mask=np.isnan(pdf1))
+pdf2 = np.ma.array(pdf2, mask=np.isnan(pdf2))
+
+ax.set_zlim(0.0, .05)
+# ax.plot_trisurf(x_1,y_1,pdf1,cmap='viridis',linewidth=0,antialiased=False,edgecolor='none')
+# ax.plot_trisurf(x_2,y_2,pdf2,cmap='magma',linewidth=0,antialiased=False,edgecolor='none')
+ax.plot_surface(x_1,y_1,pdf1,cmap='viridis',linewidth=0,antialiased=False,edgecolor='none',vmin=np.nanmin(pdf1), vmax=np.nanmax(pdf1))
+ax.plot_surface(x_2,y_2,pdf2,cmap='magma',linewidth=0,antialiased=False,edgecolor='none',vmin=np.nanmin(pdf2), vmax=np.nanmax(pdf2))
+ax.scatter3D(hp_x,hp_y,.01)
+# cmap = plt.cm.jet
+# cmap.set_bad(alpha=0.0)
+plt.show()
+
+
+###################################################
+# Question 2d
+###################################################
+dim = 1000
+data1 = np.random.multivariate_normal([8,2],[[4.1,0.4],[0.4,2.8]],dim).T
+data2 = np.random.multivariate_normal([2,8],[[4.1,0.4],[0.4,2.8]],dim).T
+
+cov1 = cov(data1)
+cov2 = cov(data2)
+
+print('Sampling PDFs...')
+x_1 = np.arange(-5, 15, .5)
+x_2 , y_1, y_2 = x_1 , x_1, x_1
+pdf1 = []
+pdf2 = []
+
+for x1 in x_1:
+    temp = []
+    for x2 in y_1:
+        temp.append(pdf([x1,x2],data1,cov1))
+    pdf1.append(temp)
+
+for x1 in x_2:
+    temp = []
+    for x2 in y_2:
+        temp.append(pdf([x1,x2],data2,cov2))
+    pdf2.append(temp)
+
+pdf1 = np.array(pdf1)
+pdf2 = np.array(pdf2)
+
+# clip data which is below 0.0
+pdf1[pdf1 < 0.0001] = np.NaN
+pdf2[pdf2 < 0.0001] = np.NaN
+
+print('Done.')
+
+x_1 = np.outer(x_1, np.ones(40))
+x_2 = np.outer(x_2, np.ones(40))
+y_1 = np.outer(np.ones(40),y_1)
+y_2 = np.outer(np.ones(40),y_2)
+
+
+# Plot hyperplane
+hp_y = []
+hp_x = np.arange(-5, 15, 1)
+for xi in hp_x:
+    hp_y.append(case2_line(xi,data1,data2))
+
+
+ax = plt.axes(projection='3d')
+pdf1 = np.ma.array(pdf1, mask=np.isnan(pdf1))
+pdf2 = np.ma.array(pdf2, mask=np.isnan(pdf2))
+
+ax.set_zlim(0.0, .05)
+ax.plot_surface(x_1,y_1,pdf1,cmap='viridis',linewidth=0,antialiased=False,edgecolor='none',vmin=np.nanmin(pdf1), vmax=np.nanmax(pdf1))
+ax.plot_surface(x_2,y_2,pdf2,cmap='magma',linewidth=0,antialiased=False,edgecolor='none',vmin=np.nanmin(pdf2), vmax=np.nanmax(pdf2))
+ax.scatter3D(hp_x,hp_y,.01)
+plt.show()
+
+
+
+###################################################
+# Question 2e
+###################################################
+dim = 1000
+data1 = np.random.multivariate_normal([8,2],[[2.1,1.5],[1.5,3.8]],dim).T
+data2 = np.random.multivariate_normal([2,8],[[4.1,0.4],[0.4,2.8]],dim).T
+
+cov1 = cov(data1)
+cov2 = cov(data2)
+
+print('Sampling PDFs...')
+x_1 = np.arange(-5, 15, .5)
+x_2 , y_1, y_2 = x_1 , x_1, x_1
+pdf1 = []
+pdf2 = []
+
+for x1 in x_1:
+    temp = []
+    for x2 in y_1:
+        temp.append(pdf([x1,x2],data1,cov1))
+    pdf1.append(temp)
+
+for x1 in x_2:
+    temp = []
+    for x2 in y_2:
+        temp.append(pdf([x1,x2],data2,cov2))
+    pdf2.append(temp)
+
+pdf1 = np.array(pdf1)
+pdf2 = np.array(pdf2)
+
+# clip data which is below 0.0
+pdf1[pdf1 < 0.0001] = np.NaN
+pdf2[pdf2 < 0.0001] = np.NaN
+
+print('Done.')
+
+x_1 = np.outer(x_1, np.ones(40))
+x_2 = np.outer(x_2, np.ones(40))
+y_1 = np.outer(np.ones(40),y_1)
+y_2 = np.outer(np.ones(40),y_2)
+
+
+# Plot hyperplane
+hp_y = []
+hp_x = np.arange(-5, 15, .5)
+for xi in hp_x:
+    hp_y.append(np.sum(case3(xi,[data1,data2],1)))
+
+hp_y = np.array(hp_y)
+hp_y[hp_y > .0001] = np.NaN
+
+
+ax = plt.axes(projection='3d')
+pdf1 = np.ma.array(pdf1, mask=np.isnan(pdf1))
+pdf2 = np.ma.array(pdf2, mask=np.isnan(pdf2))
+
+ax.set_zlim(0.0, .05)
+ax.plot_surface(x_1,y_1,pdf1,cmap='viridis',linewidth=0,antialiased=False,edgecolor='none',vmin=np.nanmin(pdf1), vmax=np.nanmax(pdf1))
+ax.plot_surface(x_2,y_2,pdf2,cmap='magma',linewidth=0,antialiased=False,edgecolor='none',vmin=np.nanmin(pdf2), vmax=np.nanmax(pdf2))
+ax.scatter3D(hp_x,hp_y,.01)
+plt.show()
+
+
 
 
 # Generate random samples
